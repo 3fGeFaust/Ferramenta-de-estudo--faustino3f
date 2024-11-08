aa6f165b3cc8b8756acb4669dc0565bbbbe0eeeb
3fGeFaust <00001100660768sp@al.educacao.sp.gov.br>
2024-11-08 10:51:30 -0300

    Add files via upload

---
+++ ferramenta de estudos- Faustino/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'O html é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da bahia?',
+    'A capital da bahia é Salvador'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'tchau em ingles é bye '
+)
